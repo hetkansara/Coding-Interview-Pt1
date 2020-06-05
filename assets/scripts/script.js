f10fe8978f0a35ad9e0bbe5ab9bda73a5df1db46
hetkansara <hetkansara.work@gmail.com>
2020-06-05 16:41:59 -0400

    DOM used instead of using html string in script file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,38 +23,59 @@ xmlhttp.send();
  * @param employees: Employees array 
  */
 function displayEmployees(employees) {
-  let htmlString = getHTMLString(employees);
-  document.getElementById('employees-container').innerHTML = sanitizeHTML(htmlString);
+  let htmlElements = getHTMLElements(employees);
+  for(let element of htmlElements){
+    document.getElementById('employees-container').appendChild(element);
+  }
 }
 
 /**
- * Returns Employee card html from employee object
+ * Returns Employee card html from the employee object
  * @param employees: Employee Object 
  */
-function getHTMLString(employees) {
-  let htmlString = "";
+function getHTMLElements(employees) {
+  let employeesDiv = [];
   for (let employee of employees) {
     console.log(employee)
-    htmlString += "<div class='info-content'>";
-    htmlString += "<p class='crown'>";
-    htmlString += (employee.employeeisfeatured == "1") ? '<img slot="crown-img" class="crown-img" src="assets/images/crown.png">' : '';;
-    htmlString += "</p><br><br>";
-    htmlString += "<div class='user-logo-container'>";
-    htmlString += '<img class="user-logo" src="http://sandbox.bittsdevelopment.com/code1/employeepics/' + employee.employeeid + '.jpg" />';
-    htmlString += "</div>";
-    htmlString += "<div>";
-    htmlString += "<h2 class='employee-name'>";
-    htmlString += employee.employeefname + ' ' + employee.employeelname;
-    htmlString += "</h2>";
-    htmlString += "<p class='employee-bio'>";
-    htmlString += employee.employeebio;
-    htmlString += "</p>";
+    let mainDiv = document.createElement("div");
+    mainDiv.classList.add('info-content');
+    let crownP = document.createElement('p');
+    crownP.classList.add('crown');
+    crownP.innerHTML = (sanitizeHTML(employee.employeeisfeatured) == "1") ? '<img slot="crown-img" class="crown-img" src="assets/images/crown.png">' : '';
+    mainDiv.appendChild(crownP);
+    mainDiv.appendChild(document.createElement('br'));
+    mainDiv.appendChild(document.createElement('br'));
+    
+    let employeeLogo = document.createElement('div');
+    employeeLogo.classList.add('user-logo-container');
+    let employeeImage = document.createElement('img');
+    employeeImage.classList.add('user-logo');
+    employeeImage.setAttribute('src', "http://sandbox.bittsdevelopment.com/code1/employeepics/" + sanitizeHTML(employee.employeeid) + ".jpg");
+    employeeLogo.appendChild(employeeImage);
+    mainDiv.appendChild(employeeLogo);
+
+    let employeeInfo = document.createElement('div');
+    let employeeName = document.createElement('h2');
+    employeeName.classList.add('employee-name');
+    employeeName.innerHTML = sanitizeHTML(employee.employeefname) + ' ' + sanitizeHTML(employee.employeelname);
+    employeeInfo.appendChild(employeeName);
+    
+    let employeeBio = document.createElement('p');
+    employeeBio.classList.add('employee-bio');
+    employeeBio.innerHTML = sanitizeHTML(employee.employeebio);
+    employeeInfo.appendChild(employeeBio);
+
     for (let role of employee.roles) {
-      htmlString += '<span class="tag" style="background-color:' + role.rolecolor + ';">' + role.rolename + '</span>';
+      let employeeBadge = document.createElement('span');
+      employeeBadge.classList.add('tag');
+      employeeBadge.style.backgroundColor = role.rolecolor;
+      employeeBadge.innerHTML = role.rolename;
+      employeeInfo.appendChild(employeeBadge);
     }
-    htmlString += "</div></div>";
+    mainDiv.appendChild(employeeInfo);
+    employeesDiv.push(mainDiv);
   }
-  return htmlString;
+  return employeesDiv;
 }
 
 
